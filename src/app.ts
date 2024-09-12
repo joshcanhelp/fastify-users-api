556417b3b7ab88ec96c0d373749ae0cce22fbfcf
Josh Cunningham <josh@joshcanhelp.com>
2024-09-12 12:15:18 -0700

    feat: initialize /clerks route

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import Fastify, { FastifyInstance, FastifyServerOptions } from "fastify";
 
 import homeRoute from "./routes/home.js";
 import populateRoute from "./routes/populate.js";
+import clerksRoute from "./routes/clerks.js";
 
 export const initFastify = (options: FastifyServerOptions = {}) => {
   const server: FastifyInstance = Fastify(options);
   server.register(homeRoute);
   server.register(populateRoute);
+  server.register(clerksRoute);
   return server;
 };
