941c18fde12ede5a9b83e1bb30e47820a9041290
Josh Cunningham <josh@joshcanhelp.com>
2024-09-12 11:01:17 -0700

    tests: test for populate request with valid authZ

--- src/routes/populate.spec.ts
+++ src/routes/populate.spec.ts
@@ -1,4 +1,5 @@
 import { initFastify } from "../app.js";
+import { makeJwt, validClients } from "../utils/jwt.js";
 
 const server = initFastify();
 
@@ -7,6 +8,13 @@ vi.mock("axios", async () => ({
     get: () => new Promise(() => {}),
   },
 }));
+
+const signingSecret = "JWT_SIGNING_KEY";
+const validJwt = await makeJwt(
+  validClients[Math.round(Math.random())],
+  ["scope1", "scope2"],
+  signingSecret,
+);
 
 describe("Route: populate", () => {
   it("rejects requests without authorization", async () => {
@@ -42,6 +50,23 @@ describe("Route: populate", () => {
         authorization: "Bearer INVALID_JWT",
       },
     });
+    const bodyParsed = JSON.parse(response.body);
+
+    expect(response.statusCode).toEqual(401);
+    expect(bodyParsed.error).toEqual("Unauthorized");
+  });
+
+  it("seeds the users with successful authorization", async () => {
+    const response = await server.inject({
+      method: "POST",
+      url: "/populate",
+      headers: {
+        authorization: `Bearer ${validJwt}`,
+      },
+    });
+
+    console.log(response);
+
     const bodyParsed = JSON.parse(response.body);
 
     expect(response.statusCode).toEqual(401);
